abf32698ec3adf6c8f6f559b382048eb759f3bc9
wyllowyllo <105272901+wyllowyllo@users.noreply.github.com>
2023-08-17 06:52:56 +0900

    Bullet Shooting System implemented
    1. Bullet boundary
    - it's for deleting bullet objs
    
    2. Bullet prefabs
    
    3. Shooting system
    - it works by Ctrl or left_mouse btn
    
    4. Level
    - bullets are be changed by player's level

--- TopGun/Assets/Scripts/Player_move.cs
+++ TopGun/Assets/Scripts/Player_move.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,11 +7,20 @@ public class Player_move : MonoBehaviour
 {
     Animator anime;
     public float speed;
+    public float power;
 
     public bool top_collision;
     public bool bottom_collision;
     public bool left_collsion;
     public bool right_collsion;
+
+   
+    public float maxShotDelay;
+    public float curShotDelay;
+
+
+    public GameObject bulletObjA;
+    public GameObject bulletObjB;
 
 
     private void Awake()
@@ -20,12 +30,24 @@ public class Player_move : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Move();
+        Fire();
+        Reload();
+    }
+
+    private void Reload()
+    {
+        curShotDelay += Time.deltaTime;
+    }
+
+    void Move()
+    {
         float h = Input.GetAxisRaw("Horizontal");
         if ((h == 1 && right_collsion) || (h == -1 && left_collsion))
             h = 0;
 
         float v = Input.GetAxisRaw("Vertical");
-        if ((v == 1 && top_collision) ||( v == -1 && bottom_collision))
+        if ((v == 1 && top_collision) || (v == -1 && bottom_collision))
             v = 0;
 
         Vector3 curPos = transform.position;
@@ -39,7 +61,49 @@ public class Player_move : MonoBehaviour
             anime.SetInteger("Input", (int)h);
         }
     }
+    void Fire()
+    {
+        if (curShotDelay < maxShotDelay) //it doesn't work until Reloading is completed
+            return;
 
+        if (Input.GetButton("Fire1"))
+        {
+
+            switch (power)
+            {
+                case 1:
+                    GameObject bullet = Instantiate(bulletObjA, transform.position, transform.rotation); //Obj »ý¼º
+                    Rigidbody2D rigid = bullet.GetComponent<Rigidbody2D>();
+                    rigid.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+                    break;
+
+                case 2:
+                    GameObject bulletL = Instantiate(bulletObjA, transform.position+Vector3.left*0.1f, transform.rotation);
+                    GameObject bulletR = Instantiate(bulletObjA, transform.position + Vector3.right * 0.1f, transform.rotation);
+                    Rigidbody2D rigidL = bulletL.GetComponent<Rigidbody2D>();
+                    Rigidbody2D rigidR = bulletR.GetComponent<Rigidbody2D>();
+                    rigidL.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+                    rigidR.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+
+                    break;
+
+                case 3:
+                    GameObject bulletLL = Instantiate(bulletObjA, transform.position+Vector3.left*0.35f, transform.rotation);
+                    GameObject bulletRR = Instantiate(bulletObjA, transform.position + Vector3.right * 0.35f, transform.rotation);
+                    GameObject bulletCC = Instantiate(bulletObjB, transform.position, transform.rotation);
+                    Rigidbody2D rigidLL = bulletLL.GetComponent<Rigidbody2D>();
+                    Rigidbody2D rigidRR = bulletRR.GetComponent<Rigidbody2D>();
+                    Rigidbody2D rigidCC = bulletCC.GetComponent<Rigidbody2D>();
+                    rigidLL.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+                    rigidRR.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+                    rigidCC.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+                    break;
+            }
+            
+
+            curShotDelay = 0;
+        }
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Border")
